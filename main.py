e7e9f9398f2cb13151a5ae2632f120520a4550ed
SpectrixOfficial <owenhutch123@gmail.com>
2018-08-27 16:23:37 +0000

    Sounds bad but it works, will get more complex

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ import pydub
 from pydub import AudioSegment
 
 print("Starting 1")
-pydub.AudioSegment.converter = r"C:\ffmpeg\bin\ffmpeg"
+song1 = AudioSegment.from_mp3("songs/wii_shop.mp3")
 
-song1 = AudioSegment.from_mp3("songs\hotline_bling.mp3")
-song2 = AudioSegment.from_mp3("songs\hotline_bling.mp3")
-
+song2 = AudioSegment.from_mp3("songs/hotline_bling.mp3")
 print("Starting 2")
-
-final = song1 + song2
-
+final = song2.overlay(song1, position=27500)
 print("Starting 3")
 
 final.export("mashup.mp3", format="mp3", tags={'artist': 'Spectrix', 'album': 'MashUpBot', 'comments': 'https://github.com/SpectrixOfficial/MashUpBot'})
